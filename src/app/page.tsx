186ee1e8f5c07d3b645ddba3f2936d121bea1fdf
Elliott <elliott@mtx.studio>
2023-10-31 22:28:09 +0100

    Your commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
  return (
  <main className="flex min-h-screen flex-col items-center justify-between p-24">
  <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
- <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+ <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
  Get started by editing&nbsp;
  <code className="font-mono font-bold">src/app/page.tsx</code>
  </p>
@@ -42,13 +42,16 @@ export default function Home() {
  <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
  <a
  href="https://www.google.com"
- className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-red-500 hover:bg-red-500 hover:dark:border-red-700 hover:dark:bg-red-700/30"
+ className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-red-500 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
  target="_blank"
  rel="noopener noreferrer"
+ style={{backgroundColor: 'red', display: 'block', width: '100%', height: '100%'}}
  >
- <h2 className={`mb-3 text-2xl font-semibold`}>Go to Google</h2>
+ <h2 className={`mb-3 text-2xl font-semibold`}>
+ Go to Google
+ </h2>
  </a>
- </div>
+</div>
  </main>
  )
 }
