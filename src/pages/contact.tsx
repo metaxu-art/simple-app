d7e1a29c7faf083b39116693793bb93b856185a2
undefined <undefined>
2023-12-07 16:35:59 +0100

    Your commit message

---
+++ src/pages/contact.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-5xl font-bold">Let's talk turkey! 🦃</h1>
+      <p className="mt-4 text-lg">Have a brilliant idea or just want to chat? Tweet us at <a href="https://twitter.com/fakehandle" className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">@fakehandle</a>!</p>
+    </main>
+  );
+}
