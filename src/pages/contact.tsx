dac87fb1f1823290f14091c5f6973310f2fe4501
undefined <undefined>
2023-12-08 16:51:27 +0100

    Your commit message

---
+++ src/pages/contact.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div className='container mx-auto px-4'>
+      <h1 className='text-4xl font-bold text-center my-12'>Get in touch!</h1>
+      <p className='text-xl text-center'>We'd love to hear from you. Shoot us an owl or send a raven to @MagicalSupport!</p>
+    </div>
+  );
+}
