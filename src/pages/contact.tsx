fcaf2d5f0532a3dc1c05f92da5ec81b40fb533c7
undefined <undefined>
2023-12-08 16:34:57 +0100

    Your commit message

---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-4xl font-bold my-8">Contact Us</h1>
+      <p className="text-lg mb-4">Got a burning question? Want to chat about life, the universe, and everything? Don't be shy - reach out!</p>
+      <p className="text-lg mb-4">Hit us up on Twitter at <a href="https://twitter.com/fake_handle" target="_blank" className="text-blue-500">@fake_handle</a> where we tweet about our love for code and cats!</p>
+    </div>
+  );
+}
