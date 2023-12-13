741eda6c0f8addc64876d8700d57b26231327aaa
undefined <undefined>
2023-12-13 12:31:24 +0100

    Your commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,3 +111,33 @@ export default function Home() {
     </main>
   )
 }
+
+original_code:
+return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      // ... existing code ...
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        // ... existing code ...
+
+updated_code:
+return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      // ... existing code ...
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        // ... existing code ...
+
+        <Link href="/contact">
+          <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Contact Us
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Get in touch with us.
+            </p>
+          </a>
+        </Link>
